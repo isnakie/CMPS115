d069d3197cc85c158711e4960b6affad8cb10bc1
derrickdo515 <dpdo@ucsc.edu>
2016-04-14 14:17:23 -0700

    Added monthly
    Added monthly

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,7 +1,10 @@
 import math
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 principle = int( input("Starting principle: "))
 interest = int( input("Your interest rate: "))
+month = int( input("How much added per month? "))
 number = int( input("Number of compounts per year: "))
 years = int( input("How many number of years: "))
 
@@ -10,10 +13,12 @@ percentage = interest/100
 a = [];
 b = [];
 
-def interestrates(p,i,n,t):
+def interestrates(p,i,m,n,t):
+	compound = p;
 	for x in range(1, t):
 		add = pow((1+(i/n)), (n*x) )
-		value = p*add
+		value = compound*add
+		compound = p + m;
 		a.append(x);
 		b.append(value);
 	return(value)
@@ -38,5 +43,5 @@ layout = dict(	title = 'Compound interest',
 fig = dict( data=data, layout=layout )
 py.iplot( fig, filename='styled-line' )
 
-result = interestrates(principle, percentage, number, years)
+result = interestrates(principle, percentage, month, number, years)
 print(result)
