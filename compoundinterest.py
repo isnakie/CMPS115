5eeb31d3e8a37b9df673960279631aaecb4864bb
derrickdo515 <dpdo@ucsc.edu>
2016-04-16 13:44:06 -0700

    small change
    added initial value

--- compoundinterest.py
+++ compoundinterest.py
@@ -11,19 +11,25 @@ percentage = interest/100
 
 a = [];
 b = [];
+c = [];
 
 def interestrates(p,i,m,n,t):
 	compound = p;
+	c.append(p);
 	for x in range(1, t):
 		add = pow((1+(i/n)), (n*x) )
 		value = compound*add
 		compound = p + m;
+		c.append(compound);
 		a.append(x);
 		b.append(value);
 	return(value)
 
 lines = plt.plot(a, b)
 plt.setp(lines, color='b', linewidth = 2.0)
+
+line = plt.plot(a, c)
+plt.setp(line, color = 'y', linewidth = 2.0)
 
 result = interestrates(principle, percentage, month, number, years)
 print(result)
